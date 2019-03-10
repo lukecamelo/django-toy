9a682dc269769c4dc73e3f1521f1726b63aa7d13
Luke Camelo <azathothbeats@gmail.com>
2019-03-10 18:31:45 -0400

    A whole bunch of view logic sh*t

--- polls/models.py
+++ polls/models.py
@@ -17,8 +17,8 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    choice_text = models.CharField(max_length=150)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
